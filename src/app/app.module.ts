9a406314d0313829a32c9af9cc4f37659e13ac42
lzNoter <paiwuo@hotmail.com>
2022-06-26 00:06:28 +0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { CoverComponent } from './cover/cover.component';
+
+const routes: Routes = [
+  { path: 'cover', component: CoverComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, CoverComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
